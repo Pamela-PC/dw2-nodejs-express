70b475c837cd6140165de6505092e2145ab11f90
DSM2 <fatec-dsm2@academico.intra>
2026-08-12 21:24:21 -0300

    Exercício funções

---
+++ Exercício-1-Funcoes/script.js
@@ -0,0 +1,21 @@
+document.write("Função Simples");
+
+function dadosPesoais(){
+    const dados = "<p>Nome: Pamela <br> Idade: 19 anos <br></p> "
+}
+
+document.write("Função Simples");
+
+document.write("Função com Parâmetros");
+
+document.write("Função com Retorno");
+
+document.write("Função com mais de um retorno");
+
+document.write("Função Anônima");
+
+document.write("Arrow Function com Parâmetro Único");
+
+document.write("Arrow Function mais de um  Parâmetro ");
+
+document.write("IIFE");
--- js-aula-01-funcoes-no-javascript/2-funcoes-com-retorno/script.js
+++ js-aula-01-funcoes-no-javascript/2-funcoes-com-retorno/script.js
@@ -2,24 +2,25 @@
 const n1 = 50;
 const n2 = 3;
 //Essa função gera um resultado e exporta
-function multiplicar(n1,n2){
-    return n1*n2;
+function multiplicar(n1, n2) {
+  return n1 * n2;
 }
 //EXIBINDO O RESULTADO
-    document.write(`<p>A multiplicação de ${n1} por ${n2} é igual a <strong> ${multiplicar(n1,n2)}</strong>. </p>`);
+document.write(
+  `<p>A multiplicação de ${n1} por ${n2} é igual a <strong> ${multiplicar(n1, n2)}</strong>. </p>`,
+);
 
 //FUNÇÕES COM MAIS DE UM RETORNO
 const number = 5;
 
-function parImpar(number){
-    if(number%2 == 0)
-    {
-     return "par";
-    }
-    else
-    {
-        return "impar";
-    }
+function parImpar(number) {
+  if (number % 2 == 0) {
+    return "par";
+  } else {
+    return "impar";
+  }
 }
 
-document.write(`<p>O número ${number} é <strong>${parImpar(number)}</strong>!<p/>`);
+document.write(
+  `<p>O número ${number} é <strong>${parImpar(number)}</strong>!<p/>`,
+);
