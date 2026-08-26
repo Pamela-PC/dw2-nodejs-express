9be27c62db230d36a28589b79b1ce5906f1a7618
Pamela Candido <pamelacdossantos195@gmail.com>
2026-08-26 20:13:40 -0300

    aula 03 - datas, moedas e strings

--- js-aula-02.3-classes/script.js
+++ js-aula-02.3-classes/script.js
@@ -23,4 +23,20 @@ const carroPopular = new Carro("fiat", "uno", "2012");
 //Acessando os ATRIBUTOS e MÉTODOS do objeto
 document.write(`
         <p>O carro ${carroPopular.marca}, modelo ${carroPopular.modelo}, é de ${carroPopular.ano} e quando ele buzina ele faz ${carroPopular.buzinar()}</p>`);
-  
+
+//Criando Uma nova instância: Carro Conversível
+const carroConversivel = new Carro();
+carroConversivel.marca = "Chevrolet";
+carroConversivel.modelo = "Camaro";
+carroConversivel.ano = "2026";
+
+//Adicionando um NOVO ATRIBUTO(não previsto na classe)
+carroConversivel.corNeon = "Azul";
+//Adicionando um NOVO MÉTODO(não previsto na classe)
+carroConversivel.turbo = function(){
+  return "Vrummm! O carro está acelerando!!";
+}
+
+document.write(
+  `<p>O carro ${carroConversivel.marca} ${carroConversivel.modelo} tem neon da cor ${carroConversivel.corNeon}</p>`
+);
